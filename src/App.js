da2de734096018f9a80c3f39a51836403dc66e27
DavidNabet <43647871+DavidNabet@users.noreply.github.com>
2021-06-30 13:58:50 +0200

    icon plus and minus

--- src/App.js
+++ src/App.js
@@ -7,32 +7,32 @@ import { BallSpinner } from "react-spinners-kit";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState({});
+	const [isLoading, setIsLoading] = useState(true);
 
-  const fetchData = async () => {
-    const response = await axios.get(
-      "https://deliveroo-back-project.herokuapp.com/"
-    );
-    setData(response.data);
-    setIsLoading(false);
-  };
+	const fetchData = async () => {
+		const response = await axios.get(
+			"https://deliveroo-back-project.herokuapp.com/"
+		);
+		setData(response.data);
+		setIsLoading(false);
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-  return isLoading ? (
-    <div className="loading">
-      <BallSpinner size={30} color="#00cdbd" loading={isLoading} />
-    </div>
-  ) : (
-    <>
-      <Header />
-      <Hero data={data.restaurant} />
-      <Content data={data.categories} />
-    </>
-  );
+	return isLoading ? (
+		<div className="loading">
+			<BallSpinner size={30} color="#00cdbd" loading={isLoading} />
+		</div>
+	) : (
+		<>
+			<Header />
+			<Hero data={data.restaurant} />
+			<Content data={data.categories} />
+		</>
+	);
 }
 
 export default App;
